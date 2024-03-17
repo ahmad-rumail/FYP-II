1810edd162005e3dacde3102ce4d28fcfde5c8f8
Hassam17 <hassamhameed12@gmail.com>
2024-03-17 17:48:27 +0500

    17/3/24
    Changes made to all levels with a touch of animation added

---
+++ Level 1/Assets/Scripts/Level 1/HighlightText.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class HighlightTextAndFillImage : MonoBehaviour
+{
+    public TextMeshProUGUI printText;
+    public TextMeshProUGUI levelText;
+    public Image levelImage;
+
+    private void Start()
+    {
+        StartCoroutine(HighlightTextAndFillImageCoroutine());
+    }
+
+    private IEnumerator HighlightTextAndFillImageCoroutine()
+    {
+        while (true)
+        {
+            levelText.color = Color.yellow;
+            printText.color = Color.black;
+            float fillAmount = 1;
+            while (fillAmount >= 0)
+            {
+                levelImage.fillAmount = fillAmount;
+                fillAmount -= 0.1f;
+                yield return new WaitForSeconds(0.1f);
+            }
+            yield return new WaitForSeconds(1);
+            printText.color = Color.white;
+            levelText.color = Color.white;
+            fillAmount = 0;
+            while (fillAmount <= 1)
+            {
+                levelImage.fillAmount = fillAmount;
+                fillAmount += 0.1f;
+                yield return new WaitForSeconds(0.1f);
+            }
+            yield return new WaitForSeconds(1);
+
+            if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCountInBuildSettings)
+            {
+                break;
+            }
+        }
+    }
+}
